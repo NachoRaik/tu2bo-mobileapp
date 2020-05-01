af753b951d859433148d6cd88193435973935e0a
Oli <olifer97@gmail.com>
2020-05-01 19:28:55 -0300

    Add expo-av and example of video in HomeScreen.

--- src/app/screens/HomeScreen/index.js
+++ src/app/screens/HomeScreen/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { SafeAreaView, Text } from 'react-native';
+import { Video } from 'expo-av';
 
 import styles from './styles';
 
@@ -7,6 +8,18 @@ function HomeScreen() {
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.title}>HOME!</Text>
+      <Video
+        source={{
+          uri: 'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4'
+        }}
+        rate={1.0}
+        volume={1.0}
+        isMuted={false}
+        resizeMode="cover"
+        shouldPlay
+        isLooping
+        style={{ width: 300, height: 300 }}
+      />
     </SafeAreaView>
   );
 }
