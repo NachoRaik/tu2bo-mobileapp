10cecea4e00c46f842a5ec1a4db3ee9d3d873867
Oli <olifer97@gmail.com>
2020-06-25 20:02:50 -0300

    Remove constant.

--- src/app/screens/ProfileScreen/constants.js
+++ src/app/screens/ProfileScreen/constants.js
@@ -1,19 +1,2 @@
-export const MY_VIDEOS = [
-  {
-    id: 1,
-    author: 'author 1',
-    user_id: 1,
-    title: 'a title',
-    description: 'a descrption',
-    date: '2019-01-02',
-    visibility: 'private',
-    url:
-      'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
-    thumb:
-      'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/images/BigBuckBunny.jpg',
-    likes: 100000
-  }
-];
-
 export const IMAGE =
   'https://i.ya-webdesign.com/images/default-avatar-png-18.png';
